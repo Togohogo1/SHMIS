0ee3007aa079f2ddfd42daa6f660c31e19314f8f
Kevin Wang <59260907+Togohogo1@users.noreply.github.com>
2022-01-01 15:08:59 -0500

    login page swing experimentation

--- shmis/src/main/java/com/company/pages/HealthcareApp.java
+++ shmis/src/main/java/com/company/pages/HealthcareApp.java
@@ -1,14 +1,40 @@
 package com.company.pages;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.WindowConstants;
+
 
 public class HealthcareApp extends JFrame {
     public HealthcareApp() {
         super("Simple Healthcare Management Interface for Specialists");
         this.setSize(960, 540);
+
+        this.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        Login login = new Login();
+
+        c.fill = GridBagConstraints.BOTH;
+        c.gridx = 0;
+        c.weightx = 0.5;
+        c.weighty = 1;
+        this.add(login, c);
+
+
+        JPanel dummy = new JPanel(new GridBagLayout());
+        dummy.setBackground(Color.cyan);
+        c.gridx = 1;
+        c.weightx = 0.5;
+        this.add(dummy, c);
+
         this.setLocationRelativeTo(null);  // Center of the screen when opened
-        this.setResizable(false);
+        // this.setResizable(false);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setVisible(true);
     }
--- shmis/src/main/java/com/company/pages/Login.java
+++ shmis/src/main/java/com/company/pages/Login.java
@@ -1,11 +1,42 @@
 package com.company.pages;
 
+import java.awt.GridBagLayout;
+import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Login extends JPanel {
     public Login() {
-        super();
-        this.setLayout();
+        // JLabel lb = new JLabel();
+        this.setLayout(new GridBagLayout());
+        this.setBackground(Color.gray);
+        // GridBagConstraints c = new GridBagConstraints();
+        // this.setSize(200, 540);
+
+        // JPanel topButtons = new JPanel();
+        // topButtons.setLayout(new BoxLayout(topButtons, BoxLayout.Y_AXIS));
+        // JButton button1 = new JButton("button 1");
+        // JButton button2 = new JButton("button 2");
+        // JButton button3 = new JButton("button 3");
+        // topButtons.add(button1);
+        // topButtons.add(button2);
+        // topButtons.add(button3);
+        // this.add(topButtons, BorderLayout.NORTH);
+
+
+        // JPanel bottomButtons = new JPanel();
+        // bottomButtons.setLayout(new BoxLayout(bottomButtons, BoxLayout.Y_AXIS));
+        JButton button4 = new JButton("button 4");
+        // bottomButtons.add(button4);
+        // this.add(bottomButtons, BorderLayout.SOUTH);
+        // this.add(button4);
     }
 }

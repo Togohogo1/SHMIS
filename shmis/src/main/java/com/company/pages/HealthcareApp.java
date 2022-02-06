60a5dd307b7f6960e45c2d23749b0ec5c32915ea
Kevin Wang <59260907+Togohogo1@users.noreply.github.com>
2022-02-06 07:35:49 -0500

    testing operations on close

--- shmis/src/main/java/com/company/pages/HealthcareApp.java
+++ shmis/src/main/java/com/company/pages/HealthcareApp.java
@@ -1,16 +1,20 @@
 package com.company.pages;
 
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
-public class HealthcareApp extends JFrame {
+public class HealthcareApp extends JFrame implements WindowListener {
     private JPanel login = new Login();
     private JPanel program = new Program();
 
     public HealthcareApp() {
         super("Simple Healthcare Management Interface for Specialists");
 
+        this.addWindowListener(this);
         this.add(login);  // Default (changed to program for testing purposes)
         this.setSize(1024, 576);
         this.setLocationRelativeTo(null);  // Center of the screen when opened
@@ -29,4 +33,32 @@ public class HealthcareApp extends JFrame {
         this.add(login);
         // Remember to reset some values (like current user)
     }
+
+    @Override
+    public void windowOpened(WindowEvent e) {}
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        System.out.println("Closing");
+        while (true) {
+            System.out.println("never closing >:)");
+        }
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+        System.out.println("Closed");
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {}
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {}
+
+    @Override
+    public void windowActivated(WindowEvent e) {}
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {}
 }

9e751f6536e20d7f237789c0c7412eb7cc21da72
Kevin Wang <59260907+Togohogo1@users.noreply.github.com>
2022-02-08 21:15:43 -0500

    raw patient profile

--- shmis/src/main/java/com/company/classes/Employee.java
+++ shmis/src/main/java/com/company/classes/Employee.java
@@ -2,7 +2,7 @@ package com.company.classes;
 
 public class Employee extends Person {
     public Employee() {
-        super(30, "admin_f", "admin_l", "M/F", "0", "password");
+        super(99, "Admin", "Nimda", "M/F", "0", "password");
     }
 
     public String getDesignation() {
--- shmis/src/main/java/com/company/pages/program/Profile.java
+++ shmis/src/main/java/com/company/pages/program/Profile.java
@@ -34,26 +34,59 @@ public class Profile extends JPanel implements ActionListener {
         logout = new JButton("Logout");
         logout.addActionListener(this);
 
-        JLabel firstName = new JLabel(person.getFirstName());
-        JLabel lastName = new JLabel(person.getLastName());
-        JLabel id = new JLabel(person.getId());
-        JLabel email;
-        JLabel age = new JLabel("" + person.getAge());
-        JLabel birthdate;
-        JLabel gender = new JLabel(person.getGender());
-        JLabel address;
-        JLabel telephone;
+        boolean isPatient = person.getDesignation().equals("Patient");
+        JLabel[] patientlabels = new JLabel[9];
+        JLabel[] employeeLabels = new JLabel[5];
+        JLabel[] patientDetails = new JLabel[9];
+        JLabel[] employeeDetails = new JLabel[5];
 
-        if (person.getDesignation().equals("Patient")) {
-            email = new JLabel(((Patient)person).getEmail());
-            birthdate = new JLabel(((Patient)person).getBirthdate());
-            address = new JLabel(((Patient)person).getAddress());
-            telephone = new JLabel(((Patient)person).getTelephone());
+        patientlabels[0] = employeeLabels[0] = new JLabel("First Name:");
+        patientlabels[1] = employeeLabels[1] = new JLabel("Last Name:");
+        patientlabels[2] = employeeLabels[2] = new JLabel("ID:");
+        patientlabels[3] = new JLabel("Email:");
+        patientlabels[4] = employeeLabels[3] = new JLabel("Age:");
+        patientlabels[5] = new JLabel("Birthdate:");
+        patientlabels[6] = employeeLabels[4] = new JLabel("Gender:");
+        patientlabels[7] = new JLabel("Address:");
+        patientlabels[8] = new JLabel("Telephone:");
+
+        patientDetails[0] = employeeDetails[0] = new JLabel(person.getFirstName());
+        patientDetails[1] = employeeDetails[1] = new JLabel(person.getLastName());
+        patientDetails[2] = employeeDetails[2] = new JLabel(person.getId());
+        patientDetails[3] = new JLabel();
+        patientDetails[4] = employeeDetails[3] = new JLabel("" + person.getAge());
+        patientDetails[5] = new JLabel();
+        patientDetails[6] = employeeDetails[4] = new JLabel(person.getGender());
+        patientDetails[7] = new JLabel();
+        patientDetails[8] = new JLabel();
+
+        if (isPatient) {
+            patientDetails[3] = new JLabel(((Patient)person).getEmail());
+            patientDetails[5] = new JLabel(((Patient)person).getBirthdate());
+            patientDetails[7] = new JLabel(((Patient)person).getAddress());
+            patientDetails[8] = new JLabel(((Patient)person).getTelephone());
         }
+
         // Setting sizes and styling
 
         // Positioning
+        c.insets = new Insets(5, 5, 5, 5);
 
+        for (int i = 0; i < 2; i++) {  // TODO take advantage of more ternary statements?
+            for (int j = 0; j < (isPatient ? 9 : 5); j++) {
+                c.gridx = i;
+                c.gridy = j;
+
+                if (i == 0)
+                    this.add((isPatient ? patientlabels[j] : employeeLabels[j]), c);
+                else
+                    this.add((isPatient ? patientDetails[j] : employeeDetails[j]), c);
+            }
+        }
+
+        c.gridx = 0;
+        c.gridy++;
+        c.gridwidth = 2;
         this.add(logout, c);
     }
 

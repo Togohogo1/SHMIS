f088b3987d2bc93b3ac7921ba390e6a78468b251
Kevin Wang <59260907+Togohogo1@users.noreply.github.com>
2022-02-13 21:15:23 -0500

    started queuetablemodel implementation

--- shmis/src/main/java/com/company/pages/program/tablemodels/QueueTableModel.java
+++ shmis/src/main/java/com/company/pages/program/tablemodels/QueueTableModel.java
@@ -1,5 +1,36 @@
 package com.company.pages.program.tablemodels;
 
-public class QueueTableModel {
+import java.util.ArrayList;
+
+import javax.swing.table.AbstractTableModel;
+
+import com.company.App;
+import com.company.classes.Appointment;
+import com.company.pages.program.Queue;
+
+public class QueueTableModel extends AbstractTableModel {
+    private String[] columnNames = {"ID", "Date", "From", "To", "Status"};
+    private Queue queue = new Queue();
+
+    public QueueTableModel(Queue queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public int getRowCount() {
+        return 0;
+    }
+
+    @Override
+    public int getColumnCount() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }

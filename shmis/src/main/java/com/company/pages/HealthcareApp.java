f3fe166e8cfa8a1f110a52a425162eaf939410e3
Kevin Wang <59260907+Togohogo1@users.noreply.github.com>
2022-02-26 13:02:21 -0500

    Update HealthcareApp.java

--- shmis/src/main/java/com/company/pages/HealthcareApp.java
+++ shmis/src/main/java/com/company/pages/HealthcareApp.java
@@ -66,20 +66,26 @@ public class HealthcareApp extends JFrame implements WindowListener {
     }
 
     @Override
-    public void windowOpened(WindowEvent e) {}
+    public void windowOpened(WindowEvent e) {
+    }
 
     @Override
-    public void windowClosed(WindowEvent e) {}
+    public void windowClosed(WindowEvent e) {
+    }
 
     @Override
-    public void windowIconified(WindowEvent e) {}
+    public void windowIconified(WindowEvent e) {
+    }
 
     @Override
-    public void windowDeiconified(WindowEvent e) {}
+    public void windowDeiconified(WindowEvent e) {
+    }
 
     @Override
-    public void windowActivated(WindowEvent e) {}
+    public void windowActivated(WindowEvent e) {
+    }
 
     @Override
-    public void windowDeactivated(WindowEvent e) {}
+    public void windowDeactivated(WindowEvent e) {
+    }
 }
